9b1c8f0dec08c91e8deb13c4652d05b1357d03a0
Victor Lima <victorandradelima@gmail.com>
2024-03-08 10:03:46 -0300

    First commit: Install Rollup and first test component

---
+++ src/components/ui/thing.tsx
@@ -0,0 +1,17 @@
+import React, { CSSProperties } from "react";
+
+export interface ThingProps {
+  name: string;
+}
+
+const Thing: React.FC<ThingProps> = ({ name }) => {
+  const style: CSSProperties = {
+    color: "red",
+    backgroundColor: "blue",
+    padding: "10px",
+    boxShadow: "0 0 10px rgba(0,0,0,0.5)",
+  };
+  return <div style={style}>{name}</div>;
+};
+
+export default Thing;
