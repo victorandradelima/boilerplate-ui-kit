5512936d0009470a799e80dd1893867c3cb49092
Victor Lima <victorandradelima@gmail.com>
2024-03-08 13:26:35 -0300

    Add Eslint and editor config

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourceMap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourceMap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts.default()],
+  },
+];
